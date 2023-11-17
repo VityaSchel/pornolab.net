e9a7eab27af08d150f88f412fa50a7c81cd3b9e4
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2023-11-17 06:59:00 +0400

    Moving from ESM to ES6, add check for token validity in setAuthToken method

--- api-wrapper/src/methods/set-auth-token.ts
+++ api-wrapper/src/methods/set-auth-token.ts
@@ -1,5 +1,7 @@
-import { PornolabAPI } from '@/index.js'
+import { InvalidAuthTokenError, PornolabAPI } from '@/index.js'
+import { authTokenRegex } from '@/utils.js'
 
 export function SetAuthToken(this: PornolabAPI, authOptions: { bbData: string }) {
+  if(!authTokenRegex.test(authOptions.bbData)) throw new InvalidAuthTokenError(authOptions.bbData)
   this.bbData = authOptions.bbData
 }
